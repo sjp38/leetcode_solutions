07e79f94b222607d403ea5820f151323504446a4
SeongJae Park <sj38.park@gmail.com>
2019-04-09 22:04:44 +0900

    Add a solution for "33. Search in Rotated Sorted Array"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 33_search_in_rotated_sorted_array/solution.c
@@ -0,0 +1,31 @@
+int idx_min(int *nums, int numsSize) {
+	int i;
+	int ret = -1;
+
+	ret = 0;
+	for (i = 1; i < numsSize; i++) {
+		if (nums[i] < ret)
+			ret = i;
+	}
+
+	return ret;
+}
+
+int search(int* nums, int numsSize, int target) {
+	int idx_m;
+	int i, cursor;
+
+	if (numsSize == 0)
+		return -1;
+
+	idx_m = idx_min(nums, numsSize);
+
+	for (i = 0; i < numsSize; i++) {
+		cursor = idx_m + i;
+		if (cursor >= numsSize)
+			cursor -= numsSize;
+		if (nums[cursor] == target)
+			return cursor;
+	}
+	return -1;
+}
