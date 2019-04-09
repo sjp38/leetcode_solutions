657dfc23fe9ed5a6dcd9fc80e7eec9e1a62d52b1
SeongJae Park <sj38.park@gmail.com>
2019-04-09 22:33:47 +0900

    33: Apply binary search
    This commit applies the binary search for final search.  However, it
    still has O(n) time complexity because the search of the start index has
    the O(n) time complexity.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 33_search_in_rotated_sorted_array/solution.c
+++ 33_search_in_rotated_sorted_array/solution.c
@@ -11,6 +11,29 @@ int idx_min(int *nums, int numsSize) {
 	return ret;
 }
 
+#define realidx(idx, start, len) (idx + start >= len ? idx + start - len : idx + start)
+
+int __search(int *nums, int nr_nums, int real_start, int target) {
+	int start, end, mid;
+	int v;
+
+	start = 0;
+	end = nr_nums - 1;
+
+	while (start <= end) {
+		mid = (start + end) / 2;
+		v = nums[realidx(mid, real_start, nr_nums)];
+		if (v == target)
+			return realidx(mid, real_start, nr_nums);
+
+		if (v < target)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	return -1;
+}
+
 int search(int* nums, int numsSize, int target) {
 	int idx_m;
 	int i, cursor;
@@ -20,15 +43,5 @@ int search(int* nums, int numsSize, int target) {
 
 	idx_m = idx_min(nums, numsSize);
 
-	for (i = 0; i < numsSize; i++) {
-		cursor = idx_m + i;
-		if (cursor >= numsSize)
-			cursor -= numsSize;
-
-		if (nums[cursor] > target)
-			return -1;
-		else if (nums[cursor] == target)
-			return cursor;
-	}
-	return -1;
+	return __search(nums, numsSize, idx_m, target);
 }
