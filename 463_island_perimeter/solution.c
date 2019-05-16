65c23977267206e404c8aeb60c0db543523116ea
SeongJae Park <sj38.park@gmail.com>
2019-05-16 18:06:26 +0900

    Add a solution for "463. Island Perimeter"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 463_island_perimeter/solution.c
@@ -0,0 +1,48 @@
+int **cells;
+int nr_rows;
+int nr_cols;
+
+/* Returns 1 if the cell is water */
+char iswater(int row, int col) {
+    if (row < 0 || row >= nr_rows || col < 0 || col >= nr_cols)
+        return 1;
+    if (cells[row][col] == 0)
+        return 1;
+    return 0;    
+}
+
+/* Returns perimeter of given ground cell */
+int perimeterof(int row, int col) {
+    int ret = 0;
+
+    /* check up, down, left and right */
+    if (iswater(row - 1, col))
+        ret += 1;
+    if (iswater(row + 1, col))
+        ret += 1;
+    if (iswater(row, col - 1))
+        ret += 1;
+    if (iswater(row, col + 1))
+        ret += 1;
+
+    return ret;
+}
+
+int islandPerimeter(int** grid, int gridSize, int* gridColSize){
+    int i, j;
+    int perim = 0;
+
+    cells = grid;
+    nr_rows = gridSize;
+    nr_cols = *gridColSize;
+
+    for (i = 0; i < nr_rows; i++) {
+        for (j = 0; j < nr_cols; j++) {
+            if (grid[i][j] == 0)
+                continue;
+            perim += perimeterof(i, j);
+        }
+    }
+
+    return perim;
+}
