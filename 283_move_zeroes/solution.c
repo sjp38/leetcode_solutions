a426024a40f3586dd4c33c374150cc9f4329208e
SeongJae Park <sj38.park@gmail.com>
2019-05-22 12:37:26 +0900

    283: Optimize to be O(n) instead of O(n**2)
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 283_move_zeroes/solution.c
+++ 283_move_zeroes/solution.c
@@ -1,20 +1,16 @@
 void moveZeroes(int* nums, int numsSize){
+    /* shift each non-zeroes with first zero: O(n) */
+    int nr_zeroes = 0;
     int i, j;
-    char finished;
     
-    for (i = 0; i < numsSize - 1; i++) {
+    for (i = 0; i < numsSize; i++) {
         if (nums[i] == 0) {
-            finished = 1;
-            for (j = i +1; j < numsSize; j++) {
-                nums[j - 1] = nums[j];
-                if (nums[j] != 0)
-                    finished = 0;
-            }
-            if (finished)
-                return;
-            nums[numsSize - 1] = 0;
-            i--;
-            numsSize--;
+            nr_zeroes++;
+            continue;
         }
+        if (nr_zeroes == 0)
+            continue;
+        nums[i - nr_zeroes] = nums[i];
+        nums[i] = 0;
     }
 }
