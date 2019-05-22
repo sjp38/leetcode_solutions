bcb86bd34e6fd8cdd99682cccfffb05b3721b89d
SeongJae Park <sj38.park@gmail.com>
2019-05-23 05:00:42 +0900

    693: Speed up using loop unrolling
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 693_binary_number_with_alternating_bits/solution.c
+++ 693_binary_number_with_alternating_bits/solution.c
@@ -1,14 +1,11 @@
 bool hasAlternatingBits(int n){
-    char last_bit = -1;
+    char last_bit = n & 1;
     char now_bit;
     
+    n = n >> 1;
     while (n > 0) {
         now_bit = (n & 1);
         n = n >> 1;
-        if (last_bit == -1) {
-            last_bit = now_bit;
-            continue;
-        }
         if (now_bit == last_bit)
             return false;
         last_bit = now_bit;
