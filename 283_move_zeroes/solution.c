2b55f6a42d49b6048af25a84df1dafcad85a76d1
SeongJae Park <sj38.park@gmail.com>
2019-05-22 12:26:48 +0900

    283: Optimize code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 283_move_zeroes/solution.c
+++ 283_move_zeroes/solution.c
@@ -1,11 +1,17 @@
 void moveZeroes(int* nums, int numsSize){
     int i, j;
+    char finished;
     
     for (i = 0; i < numsSize - 1; i++) {
         if (nums[i] == 0) {
+            finished = 1;
             for (j = i +1; j < numsSize; j++) {
-                nums[j - 1] = nums[j];   
+                nums[j - 1] = nums[j];
+                if (nums[j] != 0)
+                    finished = 0;
             }
+            if (finished)
+                return;
             nums[numsSize - 1] = 0;
             i--;
             numsSize--;
