18db8c0d3e8fa90455be500e0d464f0add66a98a
SeongJae Park <sj38.park@gmail.com>
2019-05-27 00:17:22 +0900

    231: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 231_power_of_two/solution.c
+++ 231_power_of_two/solution.c
@@ -1,12 +1,9 @@
 bool isPowerOfTwo(int n){
     if (n == 0)
         return false;
-    while (n) {
-        if (n == 1)
-            return true;
-        if (n % 2)
-            return false;
+    while (n % 2 == 0)
         n /= 2;
-    }
-    return true;
+    if (n == 1)
+        return true;
+    return false;
 }
