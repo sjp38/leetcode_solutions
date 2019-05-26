27ead44a186c053da7462433c2dae255787e690d
SeongJae Park <sj38.park@gmail.com>
2019-05-27 00:20:37 +0900

    231: Speedup using bit operation
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 231_power_of_two/solution.c
+++ 231_power_of_two/solution.c
@@ -2,7 +2,7 @@ bool isPowerOfTwo(int n){
     if (n == 0)
         return false;
     while (n % 2 == 0)
-        n /= 2;
+        n = n >> 1;
     if (n == 1)
         return true;
     return false;
