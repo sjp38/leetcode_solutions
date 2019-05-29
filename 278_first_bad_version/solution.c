b58df72857bcaff0d902c295ae62da7e24f9f31f
SeongJae Park <sj38.park@gmail.com>
2019-05-29 13:46:17 +0900

    Add a solution for "278. First Bad Version"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 278_first_bad_version/solution.c
@@ -0,0 +1,19 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    long left, right, mid;
+    
+    for (left = 1, right = n; ;) {
+        mid = (left + right) / 2;
+        if ((mid == 1 || !isBadVersion(mid - 1)) && isBadVersion(mid))
+            return mid;
+        if (!isBadVersion(mid) && (mid == n - 1 || isBadVersion(mid + 1)))
+            return mid + 1;
+        if (isBadVersion(mid))
+            right = mid;
+        else
+            left = mid;
+    }
+    return mid + 1;
+}
