93ce0da36aa2d93e5a8530c3f9c51378a034f822
SeongJae Park <sj38.park@gmail.com>
2019-06-12 13:15:12 +0900

    389: Improve memory usage and speed
    The number of letters need to be maintained in sperated array.  This
    commit saves more memory by using only one array for the purpose.  It
    might also improve speed a little because we can now know the character
    having different number of appearance while scanning second string.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 389_find_the_difference/solution.c
+++ 389_find_the_difference/solution.c
@@ -1,15 +1,14 @@
 char findTheDifference(char * s, char * t){
-    int nr_letters_s[26] = {0,};
-    int nr_letters_t[26] = {0,};
+    int nr_letters[26] = {0,};
     char *c;
     int i;
     for (c = s; *c; c++)
-        nr_letters_s[*c - 'a']++;
-    for (c = t; *c; c++)
-        nr_letters_t[*c - 'a']++;
-    for (i = 0; i < 26; i++) {
-        if (nr_letters_s[i] != nr_letters_t[i])
-            return 'a' + i;
+        nr_letters[*c - 'a']++;
+    for (c = t; *c; c++) {
+        nr_letters[*c - 'a']--;
+        if (nr_letters[*c - 'a'] < 0) {
+            return *c;
+        }
     }
     return NULL;
 }
