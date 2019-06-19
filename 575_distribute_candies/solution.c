e844b85c0d73b676c9871219d02a8428f2141cca
SeongJae Park <sj38.park@gmail.com>
2019-06-19 13:28:47 +0900

    575: Accelerate speed by skipping unnecessary kinds counting
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 575_distribute_candies/solution.c
+++ 575_distribute_candies/solution.c
@@ -6,7 +6,7 @@ int distributeCandies(int* candies, int candiesSize){
     int nr_kinds = 0, last_kind;
     int i;
     qsort(candies, candiesSize, sizeof(int), intcmp);
-    for (i = 0; i < candiesSize; i++) {
+    for (i = 0; i < candiesSize && nr_kinds < candiesSize / 2; i++) {
         if (nr_kinds == 0) {
             nr_kinds++;
             last_kind = candies[i];
@@ -17,7 +17,5 @@ int distributeCandies(int* candies, int candiesSize){
             last_kind = candies[i];
         }
     }
-    if (nr_kinds > candiesSize / 2)
-        return candiesSize / 2;
     return nr_kinds;
 }
