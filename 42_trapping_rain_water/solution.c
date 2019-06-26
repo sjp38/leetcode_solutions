da039b53a800c57444a7560465826bfb9a8f4334
SeongJae Park <sj38.park@gmail.com>
2019-06-26 17:55:01 +0900

    42: Reduce one more variable
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 42_trapping_rain_water/solution.c
+++ 42_trapping_rain_water/solution.c
@@ -1,25 +1,22 @@
 int trap(int* height, int heightSize){
     int water = 0;
-    int left_highest, right_highest, lower;
-    int right_highest_x = 1;
+    int left_highest, right_highest = 1, lower;
     int i, j;
     if (heightSize <= 2)
         return 0;
-    left_highest = height[0];
-    right_highest = height[right_highest_x];
+    left_highest = 0;
     for (i = 1; i < heightSize - 1; i++) {
-        if (height[i - 1] > left_highest)
-            left_highest = height[i - 1];
-        if (i >= right_highest_x) {
-            right_highest = 0;
+        if (height[i - 1] > height[left_highest])
+            left_highest = i - 1;
+        if (i >= right_highest) {
+            right_highest = heightSize - 1;
             for (j = heightSize - 1; j > i; j--) {
-                if (height[j] > right_highest) {
-                    right_highest = height[j];
-                    right_highest_x = j;
+                if (height[j] > height[right_highest]) {
+                    right_highest = j;
                 }
             }
         }
-        lower = left_highest > right_highest ? right_highest : left_highest;
+        lower = height[left_highest] > height[right_highest] ? height[right_highest] : height[left_highest];
         if (lower <= height[i])
             continue;
         water += lower - height[i];
