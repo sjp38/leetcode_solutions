e08d1066b1fc2c85aadbef93995a77c882717832
SeongJae Park <sj38.park@gmail.com>
2019-06-26 17:37:21 +0900

    42: Skip redundant left height checks
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 42_trapping_rain_water/solution.c
+++ 42_trapping_rain_water/solution.c
@@ -2,12 +2,13 @@ int trap(int* height, int heightSize){
     int water = 0;
     int left_highest, right_highest, lower;
     int i, j;
+    if (heightSize <= 2)
+        return 0;
+    left_highest = height[0];
     for (i = 1; i < heightSize - 1; i++) {
-        left_highest = 0, right_highest = 0;
-        for (j = 0; j < i; j++) {
-            if (height[j] > left_highest)
-                left_highest = height[j];
-        }
+        right_highest = 0;
+        if (height[i - 1] > left_highest)
+            left_highest = height[i - 1];
         for (j = heightSize - 1; j > i; j--) {
             if (height[j] > right_highest)
                 right_highest = height[j];
