70b3a03be8a1ca6e3bb3d05f33220dcb38177b12
SeongJae Park <sj38.park@gmail.com>
2019-08-22 14:24:20 +0200

    1160: Accelerate speed
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 1160_find_words_that_can_be_formed_by_characters/solution.c
+++ 1160_find_words_that_can_be_formed_by_characters/solution.c
@@ -1,4 +1,4 @@
-void cnt_chr(char *word, int *ret)
+static inline void cnt_chr(char *word, int *ret)
 {
     int i;
     for (i = 0; i < strlen(word); i++)
@@ -6,15 +6,18 @@ void cnt_chr(char *word, int *ret)
 }
 
 int countCharacters(char ** words, int wordsSize, char * chars){
-    int nr_c_word[26], nr_c_chrs[26] = {0,};
+    int nr_c_word[26], nr_c_chrs[26] = {0,}, nr_c_chrs2[26];
     int i, j, ret = 0;
     bool is_good;
     cnt_chr(chars, nr_c_chrs);
     for (i = 0; i < wordsSize; i++) {
-        memset(nr_c_word, 0, sizeof(int) * 26);
-        cnt_chr(words[i], nr_c_word);
-        for (j = 0, is_good = true; j < 26; j++) {
-            if (nr_c_chrs[j] < nr_c_word[j]) {
+        for (j = 0; j < 26; j++) {
+            nr_c_chrs2[j] = nr_c_chrs[j];
+            nr_c_word[j] = 0;
+        }
+        for (is_good = true, j = 0; j < strlen(words[i]); j++) {
+            nr_c_chrs2[words[i][j] - 'a']--;
+            if (nr_c_chrs2[words[i][j] - 'a'] < 0) {
                 is_good = false;
                 break;
             }
