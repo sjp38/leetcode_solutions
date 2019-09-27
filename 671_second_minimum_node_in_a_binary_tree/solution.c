d976d23d0513abaa32b03abe42cbbf55b4513adf
SeongJae Park <sj38.park@gmail.com>
2019-09-27 19:20:08 +0200

    Add a solution for "671. Second Minimum Node In a Binary Tree"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 671_second_minimum_node_in_a_binary_tree/solution.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int min;
+int second_min;
+
+void do_cmp(int val)
+{
+    if (val == min)
+        return;
+    if (second_min == -1) {
+        second_min = val;
+        return;
+    }
+    if (val < second_min)
+        second_min = val;
+}
+
+void traverse(struct TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    do_cmp(root->val);
+    traverse(root->left);
+    traverse(root->right);
+}
+
+int findSecondMinimumValue(struct TreeNode* root){
+    if (root == NULL)
+        return -1;
+    second_min = -1;
+    min = root->val;
+    traverse(root);
+    return second_min;
+}
