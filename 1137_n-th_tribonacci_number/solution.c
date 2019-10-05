7ad4657a41d14c821e42d958dddf371d96bdaa8b
SeongJae Park <sj38.park@gmail.com>
2019-10-05 11:07:55 +0200

    1137: Shortenize code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 1137_n-th_tribonacci_number/solution.c
+++ 1137_n-th_tribonacci_number/solution.c
@@ -1,6 +1,5 @@
 int tribonacci(int n){
     static int *cache = NULL;
-    int answer;
     if (cache == NULL)
         cache = (int *)calloc(38, sizeof(int));
     if (cache[n] != 0)
@@ -9,7 +8,6 @@ int tribonacci(int n){
         return 0;
     if (n == 1 || n == 2)
         return 1;
-    answer = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
-    cache[n] = answer;
-    return answer;
+    cache[n] = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
+    return cache[n];
 }
