a0705385930b1a9cd2ceae94ef6e4112ff756487
SeongJae Park <sj38.park@gmail.com>
2019-10-06 11:37:02 +0200

    874: Shorenize code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 874_walking_robot_simulation/solution.c
+++ 874_walking_robot_simulation/solution.c
@@ -28,20 +28,19 @@ int robotSim(int* commands, int commandsSize, int** obstacles, int obstaclesSize
             next_coord[0] = coord[0] - cmd;
         else
             printf("Wrong direction %d\n", direction);
+        
         for (j = 0; j < obstaclesSize; j++) {
             obstacle = obstacles[j];
-            if (coord[1] == obstacle[1] &&
-                coord[0] < obstacle[0] && obstacle[0] <= next_coord[0]) {
-                next_coord[0] = obstacle[0] - 1;
-            } else if (coord[1] == obstacle[1] &&
-                       next_coord[0] <= obstacle[0] && obstacle[0] < coord[0]) {
-                next_coord[0] = obstacle[0] + 1;
-            } else if (coord[0] == obstacle[0] &&
-                       coord[1] < obstacle[1] && obstacle[1] <= next_coord[1]) {
-                next_coord[1] = obstacle[1] - 1;
-            } else if (coord[0] == obstacle[0] &&
-                       next_coord[1] <= obstacle[1] && obstacle[1] < coord[1]) {
-                next_coord[1] = obstacle[1] + 1;
+            if (coord[1] == obstacle[1]) {
+                if (coord[0] < obstacle[0] && obstacle[0] <= next_coord[0])
+                    next_coord[0] = obstacle[0] - 1;
+                else if (next_coord[0] <= obstacle[0] && obstacle[0] < coord[0])
+                    next_coord[0] = obstacle[0] + 1;
+            } else if (coord[0] == obstacle[0]) {
+                if (coord[1] < obstacle[1] && obstacle[1] <= next_coord[1])
+                    next_coord[1] = obstacle[1] - 1;
+                else if (next_coord[1] <= obstacle[1] && obstacle[1] < coord[1])
+                    next_coord[1] = obstacle[1] + 1;
             }
         }
         coord[0] = next_coord[0];
