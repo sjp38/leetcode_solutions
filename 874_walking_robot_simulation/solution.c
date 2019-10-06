f3b646415d5c49798934c41e933db558494d5a03
SeongJae Park <sj38.park@gmail.com>
2019-10-06 11:24:53 +0200

    Add an solution for "874. Walking Robot Simulation"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 874_walking_robot_simulation/solution.c
@@ -0,0 +1,63 @@
+int robotSim(int* commands, int commandsSize, int** obstacles, int obstaclesSize, int* obstaclesColSize){
+    int coord[2] = {0,};
+    int next_coord[2];
+    int to_move[2];
+    int direction = 0;  /* in clockwise.  0: north, 1: east, 2: south, 3: west */
+    int cmd;
+    int *obstacle;
+    int distance, max_distance = 0;
+    int i, j;
+    
+    for (i = 0; i < commandsSize; i++) {
+        cmd = commands[i];
+        //printf("cmd %d\n", cmd);
+        if (cmd == -1) {
+            direction = (direction + 1) % 4;
+            continue;
+        } else if (cmd == -2) {
+            direction = (direction + 4 - 1) % 4;
+            continue;
+        }
+        
+        to_move[0] = to_move[1] = 0;
+        if (direction == 0)
+            to_move[1] += cmd;
+        else if (direction == 1)
+            to_move[0] += cmd;
+        else if (direction == 2)
+            to_move[1] -= cmd;
+        else if (direction == 3)
+            to_move[0] -= cmd;
+        else
+            printf("Wrong direction %d\n", direction);
+        next_coord[0] = coord[0] + to_move[0];
+        next_coord[1] = coord[1] + to_move[1];
+        for (j = 0; j < obstaclesSize; j++) {
+            obstacle = obstacles[j];
+            if (coord[1] == obstacle[1] &&
+                coord[0] < obstacle[0] && obstacle[0] <= next_coord[0]) {
+                //printf("obstacle %d %d\n", obstacle[0], obstacle[1]);
+                next_coord[0] = obstacle[0] - 1;
+            } else if (coord[1] == obstacle[1] &&
+                       next_coord[0] <= obstacle[0] && obstacle[0] < coord[0]) {
+                //printf("obstacle %d %d\n", obstacle[0], obstacle[1]);
+                next_coord[0] = obstacle[0] + 1;
+            } else if (coord[0] == obstacle[0] &&
+                       coord[1] < obstacle[1] && obstacle[1] <= next_coord[1]) {
+                //printf("obstacle %d %d\n", obstacle[0], obstacle[1]);
+                next_coord[1] = obstacle[1] - 1;
+            } else if (coord[0] == obstacle[0] &&
+                       next_coord[1] <= obstacle[1] && obstacle[1] < coord[1]) {
+                //printf("obstacle %d %d\n", obstacle[0], obstacle[1]);
+                next_coord[1] = obstacle[1] + 1;
+            }
+        }
+        coord[0] = next_coord[0];
+        coord[1] = next_coord[1];
+        //printf("new coord %d %d\n", coord[0], coord[1]);
+        distance = coord[0] * coord[0] + coord[1] * coord[1];
+        if (distance > max_distance)
+            max_distance = distance;
+    }
+    return max_distance;
+}
