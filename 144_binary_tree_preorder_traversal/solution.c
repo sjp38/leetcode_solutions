54a0e8a90047a7ee18ce601a6c9ece1176b931b6
SeongJae Park <sj38.park@gmail.com>
2019-10-07 20:45:47 +0200

    144: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 144_binary_tree_preorder_traversal/solution.c
+++ 144_binary_tree_preorder_traversal/solution.c
@@ -7,9 +7,50 @@
  * };
  */
 
-int *ret_arr;
-int cap;
-int nr_nodes;
+static struct stack_node {
+    struct TreeNode *node;
+    struct stack_node *prev;
+    struct stack_node *next;
+};
+
+static struct stack_node *stack;
+
+static int *ret_arr;
+static int cap;
+static int nr_nodes;
+
+static struct stack_node *new_stacknode(struct TreeNode *node)
+{
+    struct stack_node *ret;
+    ret = (struct stack_node *)malloc(sizeof(struct stack_node));
+    ret->node = node;
+    ret->prev = NULL;
+    ret->next = NULL;
+    return ret;
+}
+
+static void push(struct TreeNode *node)
+{
+    struct stack_node *sn;
+    sn = new_stacknode(node);
+    sn->prev = stack;
+    if (stack != NULL)
+        stack->next = sn;
+    stack = sn;
+}
+
+static struct TreeNode *pop(void)
+{
+    struct stack_node *sn;
+    struct TreeNode *node;
+    if (stack == NULL)
+        return NULL;
+    sn = stack;
+    node = sn->node;
+    stack = stack->prev;
+    free(sn);
+    return node;
+}
 
 static void init_ret_arr(int capa)
 {
@@ -25,47 +66,6 @@ static void append_ret_arr(int val)
         ret_arr = (int *)realloc(ret_arr, sizeof(int) * cap);
     }
     ret_arr[nr_nodes++] = val;
-}
-
-struct stack_node {
-    struct TreeNode *node;
-    struct stack_node *prev;
-    struct stack_node *next;
-};
-
-struct stack_node *stack;
-
-struct stack_node *new_stacknode(struct TreeNode *node)
-{
-    struct stack_node *ret;
-    ret = (struct stack_node *)malloc(sizeof(struct stack_node));
-    ret->node = node;
-    ret->prev = NULL;
-    ret->next = NULL;
-    return ret;
-}
-
-void push(struct TreeNode *node)
-{
-    struct stack_node *sn;
-    sn = new_stacknode(node);
-    sn->prev = stack;
-    if (stack != NULL)
-        stack->next = sn;
-    stack = sn;
-}
-
-struct TreeNode *pop(void)
-{
-    struct stack_node *sn;
-    struct TreeNode *node;
-    if (stack == NULL)
-        return NULL;
-    sn = stack;
-    node = sn->node;
-    stack = stack->prev;
-    free(sn);
-    return node;
 }
 
 /**
