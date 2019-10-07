1fa7efc9d030742ad3d222c39b1082cfe8a9fe41
SeongJae Park <sj38.park@gmail.com>
2019-10-07 20:43:29 +0200

    144: Use iteration based solution
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 144_binary_tree_preorder_traversal/solution.c
+++ 144_binary_tree_preorder_traversal/solution.c
@@ -27,22 +27,63 @@ static void append_ret_arr(int val)
     ret_arr[nr_nodes++] = val;
 }
 
-static void traverse(struct TreeNode *node)
+struct stack_node {
+    struct TreeNode *node;
+    struct stack_node *prev;
+    struct stack_node *next;
+};
+
+struct stack_node *stack;
+
+struct stack_node *new_stacknode(struct TreeNode *node)
 {
-    if (!node)
-        return;
-    append_ret_arr(node->val);
-    traverse(node->left);
-    traverse(node->right);
-    
+    struct stack_node *ret;
+    ret = (struct stack_node *)malloc(sizeof(struct stack_node));
+    ret->node = node;
+    ret->prev = NULL;
+    ret->next = NULL;
+    return ret;
+}
+
+void push(struct TreeNode *node)
+{
+    struct stack_node *sn;
+    sn = new_stacknode(node);
+    sn->prev = stack;
+    if (stack != NULL)
+        stack->next = sn;
+    stack = sn;
+}
+
+struct TreeNode *pop(void)
+{
+    struct stack_node *sn;
+    struct TreeNode *node;
+    if (stack == NULL)
+        return NULL;
+    sn = stack;
+    node = sn->node;
+    stack = stack->prev;
+    free(sn);
+    return node;
 }
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* preorderTraversal(struct TreeNode* root, int* returnSize){
+    struct TreeNode *node;
+
     init_ret_arr(10);
-    traverse(root);
+    stack = new_stacknode(root);
+    while (stack) {
+        node = pop();
+        if (node == NULL)
+            continue;
+        append_ret_arr(node->val);
+        push(node->right);
+        push(node->left);
+    }
     *returnSize = nr_nodes;
     return ret_arr;
 }
