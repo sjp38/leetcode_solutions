09d729620500a922f38873e6a8a4b2857973a362
SeongJae Park <sj38.park@gmail.com>
2019-10-08 20:23:28 +0200

    905: Optimize for speed and memory
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 905_sort_array_by_parity/solution.c
+++ 905_sort_array_by_parity/solution.c
@@ -2,19 +2,15 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortArrayByParity(int* A, int ASize, int* returnSize){
-    int *evens;
-    int *odds;
+    int *ret;
     int i, ie, io;
-    evens = (int *)malloc(sizeof(int) * ASize);
-    odds = (int *)malloc(sizeof(int) * ASize);
-    for (i = 0, ie = 0, io = 0; i < ASize; i++) {
+    ret = (int *)malloc(sizeof(int) * ASize);
+    for (i = 0, ie = 0, io = ASize - 1; i < ASize; i++) {
         if (A[i] % 2)
-            odds[io++] = A[i];
+            ret[io--] = A[i];
         else
-            evens[ie++] = A[i];
+            ret[ie++] = A[i];
     }
-    memcpy(&evens[ie], &odds[0], sizeof(int) * io);
-    free(odds);
     *returnSize = ASize;
-    return evens;
+    return ret;
 }
