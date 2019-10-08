e34ddf42453e169c93c9dec6906aea5a80d1aace
SeongJae Park <sj38.park@gmail.com>
2019-10-08 20:26:32 +0200

    905: Remove one more local variable
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 905_sort_array_by_parity/solution.c
+++ 905_sort_array_by_parity/solution.c
@@ -3,13 +3,13 @@
  */
 int* sortArrayByParity(int* A, int ASize, int* returnSize){
     int *ret;
-    int i, ie, io;
+    int i, io;
     ret = (int *)malloc(sizeof(int) * ASize);
-    for (i = 0, ie = 0, io = ASize - 1; i < ASize; i++) {
+    for (i = 0, io = ASize - 1; i < ASize; i++) {
         if (A[i] % 2)
             ret[io--] = A[i];
         else
-            ret[ie++] = A[i];
+            ret[i - (ASize - 1 - io)] = A[i];
     }
     *returnSize = ASize;
     return ret;
