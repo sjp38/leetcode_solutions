57e297010917be9889d619a2eefe3b18a4703112
SeongJae Park <sj38.park@gmail.com>
2019-10-08 20:12:05 +0200

    482: Optimize for speed
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 482_license_key_formatting/solution.c
+++ 482_license_key_formatting/solution.c
@@ -1,36 +1,24 @@
 char * licenseKeyFormatting(char * S, int K){
     char *c, *ret;
-    int nr_dashes = 0;
-    int len_chr;
-    int len_first_group;
-    int len_ret;
+    int nr_c_in_group;
     int i, j;
-    for (i = 0; i < strlen(S); i++) {
-        c = &S[i];
-        if (*c >= 'a' && *c <= 'z')
-            *c = 'A' + (*c - 'a');
-        else if (*c == '-')
-            nr_dashes++;
-    }
-    len_chr = strlen(S) - nr_dashes;
-    len_first_group = len_chr % K;
-    if (len_first_group == 0)
-        len_first_group = K;
-    len_ret = len_chr + len_chr / K - (len_first_group == K ? 1 : 0);
-    ret = (char *)malloc(sizeof(char) * (len_ret + 1));
-    for (i = 0, j = 0; i < strlen(S); i++) {
-        c = &S[i];
-        if (*c == '-')
+    ret = malloc(sizeof(char) * (strlen(S) * 2 + 1));
+    j = strlen(S) * 2;
+    ret[j--] = '\0';
+    nr_c_in_group = 0;
+    for (i = strlen(S) - 1; i >= 0; i--) {
+        if (S[i] == '-')
             continue;
-        if (j != 0 && 
-            ((j == len_first_group) ||
-            ((j - len_first_group) % (K + 1) == 0))) {
-            ret[j++] = '-';
-            i--;
+        if (nr_c_in_group  == K) {
+            ret[j--] = '-';
+            i++;
+            nr_c_in_group = 0;
             continue;
         }
-        ret[j++] = S[i];
+        if ('a' <= S[i] && S[i] <= 'z')
+            S[i] = 'A' + (S[i] - 'a');
+        ret[j--] = S[i];
+        nr_c_in_group++;
     }
-    ret[j] = '\0';
-    return ret;
+    return &ret[j + 1];
 }
