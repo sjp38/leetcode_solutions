4ec5e97934103cdb5c45bf4be3c2a130beabd0fb
SeongJae Park <sj38.park@gmail.com>
2019-10-09 20:51:34 +0200

    Revert "28: Apply an optimization for specific case"
    This reverts commit 840449d570f8faccf91c3c34dac89605f7c0998f.

--- 28_implement_strstr/solution.c
+++ 28_implement_strstr/solution.c
@@ -1,32 +1,17 @@
 int strStr(char * haystack, char * needle){
     char *c;
     bool is_needle;
-    int first_diff;
-    int i, j, k;
+    int i, j;
     if (needle[0] == '\0')
         return 0;
     if (strlen(haystack) < strlen(needle))
         return -1;
-    for (i = 0; i < strlen(needle); i++) {
-        if (needle[i] != needle[0])
-            break;
-    }
-    first_diff = i;
     for (i = 0; i < strlen(haystack) - strlen(needle) + 1; i++) {
         if (haystack[i] == needle[0]) {
             is_needle = true;
             for (j = 1; j < strlen(needle); j++) {
                 if (haystack[i + j] != needle[j]) {
                     is_needle = false;
-                    break;
-                }
-            }
-            if (j == first_diff && haystack[i + j] == needle[0]) {
-                for (k = i + j + 1; k < strlen(haystack) - strlen(needle) + 1; k++) {
-                    if (haystack[k] == needle[0])
-                        continue;
-                    if (haystack[k] == needle[first_diff])
-                        i = k - first_diff - 1;
                     break;
                 }
             }
