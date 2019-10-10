bd72290c7f788c4d6b961a044846806b71a8e5fa
SeongJae Park <sj38.park@gmail.com>
2019-10-10 20:30:48 +0200

    860: Remove unnecessary local variable
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 860_lemonade_change/solution.c
+++ 860_lemonade_change/solution.c
@@ -1,16 +1,15 @@
 bool lemonadeChange(int* bills, int billsSize){
     int nr_changes[3] = {0,};    /* number of 5, 10, 20 dollars */
-    int bill, i;
+    int i;
     for (i = 0; i < billsSize; i++) {
-        bill = bills[i];
-        if (bill == 5) {
+        if (bills[i] == 5) {
             nr_changes[0]++;
-        } else if (bill == 10) {
+        } else if (bills[i] == 10) {
             nr_changes[1]++;
             if (nr_changes[0] == 0)
                 return false;
             nr_changes[0]--;
-        } else if (bill == 20) {
+        } else if (bills[i] == 20) {
             nr_changes[2]++;
             if (nr_changes[0] > 0 && nr_changes[1] > 0) {
                 nr_changes[0]--;
