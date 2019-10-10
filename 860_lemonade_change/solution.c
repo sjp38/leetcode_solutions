ee03ec2c2b4a6f5fcea85824d407d4a4437c9dd3
SeongJae Park <sj38.park@gmail.com>
2019-10-10 20:27:03 +0200

    Add an answer for "860. Lemonade Change"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 860_lemonade_change/solution.c
@@ -0,0 +1,31 @@
+bool lemonadeChange(int* bills, int billsSize){
+    int nr_changes[3] = {0,};    /* number of 5, 10, 20 dollars */
+    int bill, i;
+    for (i = 0; i < billsSize; i++) {
+        bill = bills[i];
+        //printf("bill: %d, changes: %d %d %d\n", bill, nr_changes[0], nr_changes[1], nr_changes[2]);
+        if (bill == 5) {
+            nr_changes[0]++;
+            continue;
+        }
+        if (bill == 10) {
+            nr_changes[1]++;
+            if (nr_changes[0] == 0)
+                return false;
+            nr_changes[0]--;
+            continue;
+        }
+        if (bill == 20) {
+            nr_changes[2]++;
+            if (nr_changes[0] > 0 && nr_changes[1] > 0) {
+                nr_changes[0]--;
+                nr_changes[1]--;
+            } else if (nr_changes[0] >= 3) {
+                nr_changes[0] -= 3;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
