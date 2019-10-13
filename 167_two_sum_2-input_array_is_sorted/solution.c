3ef14f490ec3297aa83ca382ee57a7589edaaa1b
SeongJae Park <sj38.park@gmail.com>
2019-10-13 11:54:03 +0200

    167: Use the return array
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 167_two_sum_2-input_array_is_sorted/solution.c
+++ 167_two_sum_2-input_array_is_sorted/solution.c
@@ -9,9 +9,9 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
     for (i = 0; i < numbersSize - 1; i++) {
         for (j = i + 1; j < numbersSize; j++) {
             if (numbers[i] + numbers[j] == target) {
-                numbers[0] = i + 1;
-                numbers[1] = j + 1;
-                return numbers;
+                ret[0] = i + 1;
+                ret[1] = j + 1;
+                return ret;
             }
             if (numbers[i] + numbers[j] > target) {
                 break;
