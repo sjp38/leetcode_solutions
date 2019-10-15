28c5d29d91dff5c44536933f5262952bca8380c0
SeongJae Park <sj38.park@gmail.com>
2019-10-15 20:18:53 +0200

    540: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 540_single_element_in_a_sorted_array/solution.c
+++ 540_single_element_in_a_sorted_array/solution.c
@@ -4,16 +4,15 @@ int singleNonDuplicate(int* nums, int numsSize){
         return nums[0];
     for (l = 0, r = numsSize - 1; l != r;) {
         mid = (l + r) / 2;
-        if (mid == 0 || mid == numsSize - 1)
-            return nums[mid];
-        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        if ((mid == 0 || mid == numsSize - 1) ||
+            (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]))
             return nums[mid];
         if (nums[mid] == nums[mid - 1]) {
             if (mid % 2 == 0)
                 r = mid;
             else
                 l = mid + 1;
-        } else if (nums[mid] == nums[mid + 1]) {
+        } else {
             if (mid % 2 == 0)
                 l = mid + 1;
             else
