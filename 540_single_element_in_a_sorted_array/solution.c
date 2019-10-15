d37e88bdcb936331d77541511f193daffe006c72
SeongJae Park <sj38.park@gmail.com>
2019-10-15 20:14:24 +0200

    Add a solution for "540. Single Element in a Sorted Array"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 540_single_element_in_a_sorted_array/solution.c
@@ -0,0 +1,24 @@
+int singleNonDuplicate(int* nums, int numsSize){
+    int l, r, mid;
+    if (numsSize == 1)
+        return nums[0];
+    for (l = 0, r = numsSize - 1; l != r;) {
+        mid = (l + r) / 2;
+        if (mid == 0 || mid == numsSize - 1)
+            return nums[mid];
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+            return nums[mid];
+        if (nums[mid] == nums[mid - 1]) {
+            if (mid % 2 == 0)
+                r = mid;
+            else
+                l = mid + 1;
+        } else if (nums[mid] == nums[mid + 1]) {
+            if (mid % 2 == 0)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+    }
+    return nums[l];
+}
