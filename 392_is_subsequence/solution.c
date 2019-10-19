df52f74ff383c8feecd4e91f3166d7521809c084
SeongJae Park <sj38.park@gmail.com>
2019-10-19 10:42:33 +0200

    392: Remove local variables
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 392_is_subsequence/solution.c
+++ 392_is_subsequence/solution.c
@@ -1,10 +1,9 @@
 bool isSubsequence(char * s, char * t){
-    char *ct, *cs;
-    for (ct = t, cs = s; *ct != '\0' && *cs != '\0'; ct++) {
-        if (*ct == *cs)
-            cs++;
+    for (; *t != '\0' && *s != '\0'; t++) {
+        if (*t == *s)
+            s++;
     }
-    if (*cs == '\0')
+    if (*s == '\0')
         return true;
     return false;
 }
