e40c709f6e0fbc7ed1dba0ad768f3698b9c6e11a
SeongJae Park <sj38.park@gmail.com>
2019-10-19 10:32:51 +0200

    Add a solution for "66. Plus One"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 66_plus_one/solution.c
@@ -0,0 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+    int idx_first_no_nine, offset;
+    int i;
+    int *ret;
+    for (i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] != 9)
+            break;
+    }
+    idx_first_no_nine = i;
+    offset = idx_first_no_nine == -1 ? 1 : 0;
+    *returnSize = digitsSize + offset;
+    ret = (int *)calloc(*returnSize, sizeof(int));
+    for (i = 0; i <= idx_first_no_nine; i++) {
+        ret[i + offset] = digits[i];
+    }
+    ret[idx_first_no_nine + offset]++;
+    return ret;
+}
