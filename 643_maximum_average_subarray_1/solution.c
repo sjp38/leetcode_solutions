98743467aeb39fcc8fc7047b7ec6ff4e37ed6794
SeongJae Park <sj38.park@gmail.com>
2019-10-21 20:23:45 +0200

    643: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 643_maximum_average_subarray_1/solution.c
+++ 643_maximum_average_subarray_1/solution.c
@@ -2,12 +2,11 @@ double findMaxAverage(int* nums, int numsSize, int k){
     int i;
     int sum = 0;
     int maxsum;
-    for (i = 0; i < numsSize; i++) {
-        if (i < k) {
-            sum += nums[i];
-            maxsum = sum;
-            continue;
-        }
+    for (i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    maxsum = sum;
+    for (; i < numsSize; i++) {
         sum += (nums[i] - nums[i - k]);
         if (sum > maxsum)
             maxsum = sum;
