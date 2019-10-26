764a1189a2bf335494773481bc30285868c6b486
SeongJae Park <sj38.park@gmail.com>
2019-10-26 10:59:24 +0200

    501: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 501_find_mode_in_binary_search_tree/solution.c
+++ 501_find_mode_in_binary_search_tree/solution.c
@@ -7,36 +7,16 @@
  * };
  */
 
-int max_occurrence;
-int curr_occurrence;
-int prev_value;
+static bool find_max;
+static int max_occurrence;
+static int curr_occurrence;
+static int prev_value;
 
-void get_max_occurrence(struct TreeNode *node)
-{
-    if (node == NULL)
-        return;
-    get_max_occurrence(node->left);
-    if (curr_occurrence == 0) { /* root node case */
-        prev_value = node->val;
-        curr_occurrence = 1;
-    } else {
-        if (node->val == prev_value) {
-            curr_occurrence++;
-        } else {   
-            prev_value = node->val;
-            curr_occurrence = 1;
-        }
-    }
-    if (curr_occurrence > max_occurrence)
-        max_occurrence = curr_occurrence;
-    get_max_occurrence(node->right);
-}
+static int *modes;
+static int nr_modes;
+static int cap_modes;
 
-int *modes;
-int nr_modes;
-int cap_modes;
-
-void add_mode(int mode)
+static void add_mode(int mode)
 {
     if (nr_modes == cap_modes) {
         cap_modes *= 2;
@@ -45,7 +25,7 @@ void add_mode(int mode)
     modes[nr_modes++] = mode;
 }
 
-void get_modes(struct TreeNode *node)
+static void get_modes(struct TreeNode *node)
 {
     if (node == NULL)
         return;
@@ -61,8 +41,13 @@ void get_modes(struct TreeNode *node)
             curr_occurrence = 1;
         }
     }
-    if (curr_occurrence == max_occurrence)
-        add_mode(node->val);
+    if (find_max) {
+        if (curr_occurrence > max_occurrence)
+            max_occurrence = curr_occurrence;
+    } else {
+        if (curr_occurrence == max_occurrence)
+            add_mode(node->val);
+    }
     get_modes(node->right);
 }
 
@@ -73,13 +58,15 @@ int* findMode(struct TreeNode* root, int* returnSize){
     max_occurrence = 0;
     curr_occurrence = 0;
     
-    get_max_occurrence(root);
+    find_max = true;
+    get_modes(root);
     if (modes)
         free(modes);
     nr_modes = 0;
     cap_modes = 100;
     modes = (int *)malloc(sizeof(int) * cap_modes);
     curr_occurrence = 0;
+    find_max = false;
     get_modes(root);
     *returnSize = nr_modes;
     return modes;
