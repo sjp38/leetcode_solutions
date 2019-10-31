de7690ceea71753a43b3a5d10c1401801d2a1dab
SeongJae Park <sj38.park@gmail.com>
2019-10-31 11:14:10 +0100

    901: Speed up using cached prev-result
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 901_online_stock_span/solution.c
+++ 901_online_stock_span/solution.c
@@ -1,7 +1,9 @@
 typedef struct {
     int *prices;
+    int *spans;
     int nr_prices;
     int cap_prices;
+    int min_span;
 } StockSpanner;
 
 
@@ -11,6 +13,8 @@ StockSpanner* stockSpannerCreate() {
     ret->cap_prices = 100;
     ret->nr_prices = 0;
     ret->prices = (int *)malloc(sizeof(int) * ret->cap_prices);
+    ret->spans = (int *)malloc(sizeof(int) * ret->cap_prices);
+    ret->min_span = 0;
     return ret;
 }
 
@@ -19,13 +23,16 @@ int stockSpannerNext(StockSpanner* obj, int price) {
     if (obj->nr_prices == obj->cap_prices) {
         obj->cap_prices *= 2;
         obj->prices = (int *)realloc(obj->prices, sizeof(int) * obj->cap_prices);
+        obj->spans = (int *)realloc(obj->spans, sizeof(int) * obj->cap_prices);
     }
     obj->prices[obj->nr_prices++] = price;
-    for (i = obj->nr_prices - 2, ret = 1; i >= 0; i--) {
+    for (i = obj->nr_prices - 2, ret = 1; i >= 0;) {
         if (obj->prices[i] > price)
             break;
-        ret++;
+        ret += obj->spans[i];
+        i -= obj->spans[i];
     }
+    obj->spans[obj->nr_prices - 1] = ret;
     return ret;
 }
 
