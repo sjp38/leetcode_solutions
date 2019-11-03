54cb7156fe9f530e7b1e416b3654460fb9cca8d7
SeongJae Park <sj38.park@gmail.com>
2019-11-03 12:04:20 +0100

    485: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 485_max_consecutive_ones/solution.c
+++ 485_max_consecutive_ones/solution.c
@@ -1,21 +1,19 @@
+#define max(a,b) (a > b ? a : b)
+
 int findMaxConsecutiveOnes(int* nums, int numsSize){
     int maxlen = 0;
     int cons_start_idx = -1;
     int i;
-    for (i = 0; i < numsSize; i++) {
-        if (cons_start_idx == -1 && nums[i] == 0)
-            continue;
-        if (cons_start_idx == -1 && nums[i] == 1)
-            cons_start_idx = i;
-        if (cons_start_idx != -1 && nums[i] == 0) {
-            if (maxlen < i - cons_start_idx)
-                maxlen = i - cons_start_idx;
-            cons_start_idx = -1;
+    for (i = 0; i <= numsSize; i++) {
+        if (i == numsSize || nums[i] == 0) {
+            if (cons_start_idx != -1) {
+                maxlen = max(maxlen, i - cons_start_idx);
+                cons_start_idx = -1;
+            }
+        } else {
+            if (cons_start_idx == -1)
+                cons_start_idx = i;
         }
-    }
-    if (cons_start_idx != -1) {
-        if (maxlen < i - cons_start_idx)
-            maxlen = i - cons_start_idx;
     }
     return maxlen;
 }
