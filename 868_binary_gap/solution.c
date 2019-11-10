42ee806a36909ad5bef79d17aae873a4e69faa25
SeongJae Park <sj38.park@gmail.com>
2019-11-10 10:17:52 +0100

    868: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 868_binary_gap/solution.c
+++ 868_binary_gap/solution.c
@@ -3,15 +3,11 @@ int binaryGap(int N){
     int last_one = -1;
     int max_dist;
     for (idx = 0, last_one = -1, max_dist = 0; N; idx++) {
-        if (N % 2 == 0) {
-            N /= 2;
-            continue;
+        if (N % 2) {
+            if (last_one != -1)
+                max_dist = max_dist > idx - last_one ? max_dist : idx - last_one;
+            last_one = idx;
         }
-        if (last_one != -1) {
-            if (max_dist < idx - last_one)
-                max_dist = idx - last_one;
-        }
-        last_one = idx;
         N /= 2;
     }
     return max_dist;
