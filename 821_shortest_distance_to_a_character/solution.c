c43269b1a8ffb49ebe8037c10b9abb41fa2b4b45
SeongJae Park <sj38.park@gmail.com>
2019-11-13 18:45:28 +0100

    821: Cleanup code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 821_shortest_distance_to_a_character/solution.c
+++ 821_shortest_distance_to_a_character/solution.c
@@ -10,24 +10,21 @@ int* shortestToChar(char * S, char C, int* returnSize){
     *returnSize= strlen(S);
     for (c = S; *c; c++) {
         if (*c == C) {
-            printf("%d\n", c - S);
+            if (rightidx != -1)
+                leftidx = rightidx;
+            rightidx = c - S;
             if (leftidx == -1) {
-                leftidx = c - S;
-                for (i = 0; i <= leftidx; i++)
-                    ret[i] = leftidx - i;
-                last_idx = leftidx;
+                for (i = 0; i <= rightidx; i++)
+                    ret[i] = rightidx - i;
             } else {
-                if (rightidx != -1)
-                    leftidx = rightidx;
-                rightidx = c - S;
                 for (i = last_idx + 1; i <= (leftidx + rightidx) / 2; i++) {
                     ret[i] = i - leftidx;
                 }
                 for (i = (leftidx + rightidx) / 2 + 1; i <= rightidx; i++) {
                     ret[i] = rightidx - i;
                 }
-                last_idx = rightidx;
             }
+            last_idx = rightidx;
         }
     }
     if (rightidx == -1)
