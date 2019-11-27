2c65252b5ff095ca5a963691552441a0497de071
SeongJae Park <sj38.park@gmail.com>
2019-11-27 20:04:28 +0100

    Add a solution for "92. Reverse Linked List II"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 92_reverse_linked_list_2/solution.c
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseBetween(struct ListNode* head, int m, int n){
+    struct ListNode **startlink = &head;
+    struct ListNode *prev;
+    struct ListNode *prevprev;
+    struct ListNode *node;
+    int idx;
+    for (node = head, idx = 1; node && idx <= n; node = node->next, idx++) {
+        if (idx < m - 1)
+            continue;
+        if (idx == m - 1) {
+            startlink = &node->next;
+            continue;
+        }
+        if (idx == m) {
+            prevprev = node;
+            continue;
+        }
+        if (idx == m + 1) {
+            prev = node;
+            if (idx == n) {
+                (*startlink)->next = node->next;
+                node->next = prevprev;
+                *startlink = node;
+                break;
+            }
+            continue;
+        }
+        prev->next = prevprev;
+        if (idx == n) {
+            (*startlink)->next = node->next;
+            node->next = prev;
+            *startlink = node;
+            break;
+        }
+        prevprev = prev;
+        prev = node;
+    }
+    if (m == 1)
+        return *startlink;
+    return head;
+}
