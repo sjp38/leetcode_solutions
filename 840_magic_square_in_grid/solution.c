83ae6197ceec3242a38afd94c5651a573d8e7853
SeongJae Park <sj38.park@gmail.com>
2019-12-01 09:18:56 +0100

    Add a solution for "840. Magic Squares In Grid"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 840_magic_square_in_grid/solution.c
@@ -0,0 +1,52 @@
+bool is_magic(int **grid, int row, int col)
+{
+    bool nums[9] = {0,};
+    int sum, last_sum;
+    int i, j;
+    for (i = row; i < row + 3; i++) {
+        sum = 0;
+        for (j = col; j < col + 3; j++) {
+            if (grid[i][j] < 1 || grid[i][j] > 9)
+                return false;
+            if (nums[grid[i][j] - 1])
+                return false;
+            nums[grid[i][j] - 1] = true;
+            sum += grid[i][j];
+        }
+        if (i == row)
+            last_sum = sum;
+        if (sum != last_sum)
+            return false;
+    }
+    for (j = col; j < col + 3; j++) {
+        sum = 0;
+        for (i = row; i < row + 3; i++) {
+            sum += grid[i][j];
+        }
+        if (sum != last_sum)
+            return false;
+    }
+    sum = 0;
+    for (i = 0; i < 3; i++)
+        sum += grid[row + i][col + i];
+    if (sum != last_sum)
+        return false;
+    sum = 0;
+    for (i = 0; i < 3; i++)
+        sum += grid[row - i + 2][col + i];
+    if (sum != last_sum)
+        return false;
+    return true;
+}
+
+int numMagicSquaresInside(int** grid, int gridSize, int* gridColSize){
+    int i, j;
+    int rc = 0;
+    for (i = 0; i < gridSize - 2; i++) {
+        for (j = 0; j < gridColSize[0] - 2; j++) {
+            if (is_magic(grid, i, j))
+                rc++;
+        }
+    }
+    return rc;
+}
