1c8cd2381e60b269caac5ec416f2b8ac1255da74
SeongJae Park <sj38.park@gmail.com>
2020-01-08 18:33:34 +0100

    Add a solution for "437. Path Sum III"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 437_path_sum_3/solution.c
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int answer;
+struct TreeNode **parents;
+int nr_parents;
+int cap_parents;
+
+int add_parent(struct TreeNode *node)
+{
+    if (nr_parents == cap_parents) {
+        cap_parents *= 2;
+        parents = (struct TreeNode **)realloc(parents, sizeof(struct TreeNode *) * cap_parents);
+        if (!parents)
+            return -1;
+    }
+    parents[nr_parents++] = node;
+    return 0;
+}
+
+void trav(struct TreeNode *node, int target, int parent_sum)
+{
+    int sum;
+    int i;
+    
+    if (!node)
+        return;
+    
+    sum = parent_sum + node->val;
+    if (sum == target)
+        answer++;
+    for (i = 0; i < nr_parents; i++) {
+        sum -= parents[i]->val;
+        if (sum == target)
+            answer++;
+    }
+    
+    add_parent(node);
+    trav(node->left, target, parent_sum + node->val);
+    trav(node->right, target, parent_sum + node->val);
+    
+    nr_parents--;
+}
+
+int pathSum(struct TreeNode* root, int sum){
+    answer = 0;
+    cap_parents = 10;
+    parents = (struct TreeNode **)malloc(sizeof(struct TreeNode *) * cap_parents);
+    nr_parents = 0;
+    
+    trav(root, sum, 0);
+    return answer;
+}
