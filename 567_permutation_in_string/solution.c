8c0b5f6b7095fd7e2a9f293e58a1738527993899
SeongJae Park <sj38.park@gmail.com>
2020-01-13 18:25:00 +0100

    567: Further optimize using word frequency
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 567_permutation_in_string/solution.c
+++ 567_permutation_in_string/solution.c
@@ -1,40 +1,34 @@
-int cmp_char(const void *l, const void *r)
+void count_chrs(char *s, int len, int *counts)
 {
-    return *(char *)l - *(char *)r;
-}
-
-int get_chksum(char *s, int len)
-{
-    int sum = 0, i;
-    for (i = 0; i < len; i++)
-        sum += (int)s[i];
-    return sum;
+    int i;
+    for (i = 0; i < len; i++) {
+        counts[s[i] - 'a']++;
+    }
 }
 
 bool checkInclusion(char * s1, char * s2){
     int len1, len2;
-    int sum, sum2;
-    int i;
-    char *sorted;
-    int diffs;
+    int i, j;
+    int nr_chrs1[26] = {0,};
+    int nr_chrs2[26] = {0,};
     
     len1 = strlen(s1);
     len2 = strlen(s2);
     if (len1 > len2)
         return false;
-    sum = get_chksum(s1, len1);
-    qsort(s1, len1, sizeof(char), cmp_char);
-    sorted = (char *)malloc(sizeof(char) * len1);
+    count_chrs(s1, len1, nr_chrs1);
     for (i = 0; i <= len2 - len1; i++) {
-        if (i == 0)
-            sum2 = get_chksum(&s2[i], len1);
-        else
-            sum2 += s2[i + len1 - 1] - s2[i - 1];
-        if (sum2 != sum)
-            continue;
-        memcpy(sorted, &s2[i], sizeof(char) * len1);
-        qsort(sorted, len1, sizeof(char), cmp_char);
-        if (!strncmp(sorted, s1, len1))
+        if (i == 0) {
+            count_chrs(&s2[i], len1, nr_chrs2);
+        } else {
+            nr_chrs2[s2[i + len1 - 1] - 'a']++;
+            nr_chrs2[s2[i - 1] - 'a']--;
+        }
+        for (j = 0; j < 26; j++) {
+            if (nr_chrs1[j] != nr_chrs2[j])
+                break;
+        }
+        if (j == 26)
             return true;
     }
     return false;
