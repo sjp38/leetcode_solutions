9f143bd28def46ef38a9e4e16a4f70cdf628c202
SeongJae Park <sj38.park@gmail.com>
2020-01-19 15:33:15 +0100

    160: Optimize based on given solution
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 160_intersection_of_two_linked_lists/solution.c
+++ 160_intersection_of_two_linked_lists/solution.c
@@ -7,12 +7,27 @@
  */
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
     struct ListNode *a, *b;
+    struct ListNode *a_last = NULL, *b_last = NULL;
     
-    for (a = headA; a; a = a->next) {
-        for (b = headB; b; b = b->next) {
-            if (a == b)
-                return a;
+    a = headA, b = headB;
+    while (true) {
+        if (!a || !b)
+            return NULL;
+        if (a == b)
+            return a;
+        if (!a->next) {
+            a_last = a;
+            a = headB;
+        } else {
+            a = a->next;
         }
+        if (!b->next) {
+            b_last = b;
+            b = headA;
+        } else {
+            b = b->next;
+        }
+        if (a_last && b_last && a_last != b_last)
+            return NULL;
     }
-    return NULL;
 }
