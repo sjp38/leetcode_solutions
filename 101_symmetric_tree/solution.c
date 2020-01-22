c9d27119cb29d1a69ec94f63381931bbddc5a345
SeongJae Park <sj38.park@gmail.com>
2020-01-22 19:31:59 +0100

    Add a solution for "101. Symmetric Tree"
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ 101_symmetric_tree/solution.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool is_symmetric(struct TreeNode *l, struct TreeNode *r)
+{
+    if (!l && !r)
+        return true;
+    if (!l || !r)
+        return false;
+    if (l->val != r->val)
+        return false;
+    if (!is_symmetric(l->left, r->right))
+        return false;
+    if (!is_symmetric(l->right, r->left))
+        return false;
+    return true;
+}
+
+bool isSymmetric(struct TreeNode* root){
+    return is_symmetric(root, root);
+}
