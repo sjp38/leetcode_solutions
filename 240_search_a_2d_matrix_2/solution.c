02470fd20faed506eaffe1fc9ff9e48c88a94747
SeongJae Park <sj38.park@gmail.com>
2020-01-22 20:08:22 +0100

    260: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 240_search_a_2d_matrix_2/solution.c
+++ 240_search_a_2d_matrix_2/solution.c
@@ -1,18 +1,6 @@
-bool do_slow_search(int** matrix, int row_start, int row_end, int col_start, int col_end, int target)
-{
-    int i, j;
-    for (i = row_start; i < row_end; i++) {
-        for (j = col_start; j < col_end; j++) {
-            if (matrix[i][j] == target)
-                return true;
-        }
-    }
-    return false;
-}
-
 bool do_search_matrix(int** matrix, int row_start, int row_end, int col_start, int col_end, int target)
 {
-    int i, j;
+    int i, j, k;
     for (i = row_end - 1, j = col_end - 1; i >= row_start && j >= col_start; i--, j--) {
         if (matrix[i][j] < target)
             break;
@@ -22,15 +10,19 @@ bool do_search_matrix(int** matrix, int row_start, int row_end, int col_start, i
         return false;
     
     if (i == row_end - 1) {
-        if (do_slow_search(matrix, i, row_end, col_start, j + 1, target))
-            return true;
+        for (k = col_start; k < j + 1; k++) {
+            if (matrix[i][k] == target)
+                return true;
+        }
     } else if (do_search_matrix(matrix, i, row_end, col_start, j + 1, target)) {
         return true;
     }
     
     if (j == col_end - 1) {
-        if (do_slow_search(matrix, row_start, i + 1, j, col_end, target))
-            return true;
+        for (k = row_start; k < i + 1; k++) {
+            if (matrix[k][j] == target)
+                return true;
+        }
     } else if (do_search_matrix(matrix, row_start, i + 1, j, col_end, target)) {
         return true;
     }
