c2e8e081007ac8df3f2ecbfe1d0b36382a87bfa9
SeongJae Park <sj38.park@gmail.com>
2020-01-23 11:24:25 +0100

    925: Clean up code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- 925_long_pressed_name/solution.c
+++ 925_long_pressed_name/solution.c
@@ -1,10 +1,10 @@
-
-
 bool isLongPressedName(char * name, char * typed){
     char *n, *t, *prev_t;
     if (!*name && *typed)
         return false;
-    for (n = name, t = typed, prev_t = typed; *n && *t; n++) {
+    for (n = name, t = typed, prev_t = typed; *n; n++) {
+        if (!*t)
+            return false;
         if (*n != *t) {
             while (prev_t != t && *prev_t == *t) {
                 t++;
@@ -16,8 +16,6 @@ bool isLongPressedName(char * name, char * typed){
         prev_t = t;
         t++;
     }
-    if (*n || (n == name && *t))
-        return false;
     while (*t) {
         if (*t != n[-1])
             return false;
